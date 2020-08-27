c1b7a6594c1137aecfb5bb7cf180ec7735c68066
Jeromy Hettinga <jeromy.hettinga97@casema.nl>
2020-08-27 21:19:40 +0200

    Added "copy to clipboard" option to export.

--- app/Views/pages/schedule/export.php
+++ app/Views/pages/schedule/export.php
@@ -7,11 +7,14 @@ include APPPATH . 'Views/header.php';
 	<div class="col-auto">
 		<a href="<?= site_url('schedule') ?>" class="btn btn-primary mb-3"><i class="fas fa-arrow-left"></i> Back</a>
 	</div>
+	<div class="col-auto button-group">
+		<button type="button" class="btn btn-outline-primary" onclick="copyToClipboard('export')"><i class="fas fa-copy"></i> Copy to clipboard</button>
+	</div>
 </div>
 
 <div class="row">
 	<div class="col-12 form-group">
-		<textarea class="form-control" readonly><?= $export ?></textarea>
+		<textarea id="export" class="form-control" rows="25" readonly><?= $export ?></textarea>
 	</div>
 </div>
 		
--- public/assets/js/site.js
+++ public/assets/js/site.js
@@ -5,3 +5,18 @@ $(document).ready(function(){
 	})
 	
 });
+
+function copyToClipboard(elementId) {
+	/* Get the text field */
+	var copyText = document.getElementById(elementId);
+
+	/* Select the text field */
+	copyText.select();
+	copyText.setSelectionRange(0, 99999); /*For mobile devices*/
+
+	/* Copy the text inside the text field */
+	document.execCommand("copy");
+
+	/* Log the copied text */
+	console.log("Copied the text: " + copyText.value);
+}
